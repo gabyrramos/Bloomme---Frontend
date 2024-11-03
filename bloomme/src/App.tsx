c7f7f1bea8024193d881d03caf1b707f6b7d4af3
Gladis Danilu Farill Casiano <danilu080893@hotmail.com>
2024-11-03 02:00:00 -0600

    feat: changes in the login, flashing effect

--- bloomme/src/App.tsx
+++ bloomme/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Im
 import { Home } from './views/Home.view';
 import { Login } from './views/Login.view';
 import { Quiz } from './views/Quiz.view';
+import { QuizQuestion } from './views/QuizQuestion.view';
 
 const App: React.FC = () => {
   return (
@@ -11,6 +12,7 @@ const App: React.FC = () => {
         <Route path='/' element={<Home/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/quiz' element={<Quiz/>}/>
+        <Route path='/quizQuestion' element={<QuizQuestion/>}/>
       </Routes>
     </Router>
   );
