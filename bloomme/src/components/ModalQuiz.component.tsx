07f1886ea7e467a9d181a7b82ee6851088ac7673
Gladis Danilu Farill Casiano <danilu080893@hotmail.com>
2024-11-06 18:02:30 -0600

    feat:creating modal for quiz results

---
+++ bloomme/src/components/ModalQuiz.component.tsx
@@ -0,0 +1,25 @@
+import { useNavigate } from "react-router-dom";
+import React from 'react';
+import '../styles/ModalQuiz.style.css';
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+export const ModalQuiz: React.FC<ModalProps> = ({ isOpen, children }) => {
+  const navigate = useNavigate();
+  const handleClose = () => {
+    navigate('/quiz', { replace: true }); // Reemplaza la ruta actual en el historial
+  };
+  if (!isOpen) return null;
+  return (
+    <div className="modal-overlay" data-backdrop="static">
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        {children}
+        <button className="modalQuiz-button" onClick={handleClose}>Send results</button>
+      </div>
+    </div>
+  );
+};
