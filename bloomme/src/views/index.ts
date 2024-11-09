6a5d8e98a1febcba32254720b7b3d29c35a54a05
Barbara De Abreu <0deabreubarbara@gmail.com>
2024-11-09 11:36:05 -0500

    feat: Add barrel file to the views

---
+++ bloomme/src/views/index.ts
@@ -0,0 +1,11 @@
+export { default as Chat } from './Chat.view';
+export { default as Paths } from './Paths.view';
+export { default as Register } from './Register.view';
+export { default as PathModules } from './PathModules.view';
+export * from './Quiz.view';
+export * from './Home.view';
+export * from './LandingPage.view';
+export * from './Login.view';
+export * from './QuizQuestion.view';
+export * from './Search.view';
+export * from './MyProgress.view';
