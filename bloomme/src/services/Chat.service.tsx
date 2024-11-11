d720e1753411ae04f4ed0d7c1964a86fb848256b
Barbara <0deabreubarbara@gmail.com>
2024-11-10 20:47:04 -0500

    feat: add conexion to chat

---
+++ bloomme/src/services/Chat.service.tsx
@@ -0,0 +1,29 @@
+export const chatPost = async(message:string, token:string) => {
+  try {
+    const response = await fetch(
+      "https://bloomme-backend.onrender.com/api/chat",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`,
+        },
+        body: JSON.stringify({ message }),
+      },
+    );
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.message);
+    }
+
+    const resData = await response.json();
+    console.log(resData);
+    return resData;
+  } catch (error) {
+    console.log({ error });
+    const errorMessage =
+      error instanceof Error ? error.message : "Error inesperado";
+    throw new Error(errorMessage);
+  }
+};
