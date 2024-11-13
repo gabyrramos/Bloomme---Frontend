55a7b1742f84102bb79fe5858a24a171c2422c92
Barbara <0deabreubarbara@gmail.com>
2024-11-13 00:50:52 -0500

    feat: user update modal service

--- bloomme/src/services/userUpdateModal.service.tsx
+++ bloomme/src/services/userUpdateModal.service.tsx
@@ -1,17 +1,19 @@
 const userUpdateModal = async ({
-    username,
-    password,
-    age,
-    country,
-    assistant_name,
-  }: FormData) => {
-    try {
-      const token = localStorage.getItem('token');
-      const response = await fetch(`https://bloomme-backend.onrender.com/api/user/`, {
-        method: 'PUT',
+  username,
+  password,
+  age,
+  country,
+  assistant_name,
+}: FormData) => {
+  try {
+    const token = localStorage.getItem("token");
+    const response = await fetch(
+      `https://bloomme-backend.onrender.com/api/user/`,
+      {
+        method: "PUT",
         headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`,
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify({
           username,
@@ -20,16 +22,18 @@ const userUpdateModal = async ({
           country,
           assistant_name,
         }),
-      });
-      if (!response.ok) {
-        throw new Error('Error al actualizar el usuario');
-      }
-      const data = await response.json();
-      console.log('Usuario actualizado correctamente', data);
-    } catch (error) {
-      const errorMessage = error instanceof Error ? error.message : 'Error inesperado';
-      throw new Error(errorMessage);
+      },
+    );
+    if (!response.ok) {
+      throw new Error("Error al actualizar el usuario");
     }
-  };
+    const data = await response.json();
+    console.log("Usuario actualizado correctamente", data);
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error ? error.message : "Error inesperado";
+    throw new Error(errorMessage);
+  }
+};
 
-  export default userUpdateModal
+export default userUpdateModal;
