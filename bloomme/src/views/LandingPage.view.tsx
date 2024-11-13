ed5f074db0027e5dad14692ec0e54bc8ce58847c
Barbara <0deabreubarbara@gmail.com>
2024-11-13 00:52:14 -0500

    feat: header style and general styles

--- bloomme/src/views/LandingPage.view.tsx
+++ bloomme/src/views/LandingPage.view.tsx
@@ -1,11 +1,10 @@
 //import rabbitWink from '../assets/rabbitWink.png';
 import { LandingPage } from "../components/FrontPage/LandingPage.component";
 
-
 export const LandingPageView = () => {
- return (
-<div className="landingpage-body">
- <LandingPage />
-</div>
- );
-}
+  return (
+    <div className="landingpage-body">
+      <LandingPage />
+    </div>
+  );
+};
