35b03a9704a7e29a64ac911b1403b99aa18c1ee8
Barbara <0deabreubarbara@gmail.com>
2024-11-13 00:37:56 -0500

    feat: images added for register and login, change in styles

--- bloomme/src/views/Login.view.tsx
+++ bloomme/src/views/Login.view.tsx
@@ -32,19 +32,18 @@ export const Login = () => {
     <>
       <main className='container-main-login'>
         <div className='container-main-login-div'>
-          <h1 className='container-main-login-title'>SIGN IN</h1>
-          <p className='container-main-login-text'> Don't have an account? <Link to='/register' className='container-main-login-link'>Sign up</Link></p>
-          <figure className='container-login-image'>
+          <h1 className='container-main-login-title text-5xl mt-48'>SIGN IN</h1>
+          <p className='container-main-login-text mt-4'> Don't have an account? <Link to='/register' className='container-main-login-link'>Sign up</Link></p>
+          <figure className='container-login-image my-10'>
             <img src={google} alt="google" className='icon-google-login'/>
             <img src={facebook} alt="facebook" className='icon-google-login'/>
             <img src={instagram} alt="user" className='icon-google-login'/>
           </figure>
-          <form action="login" className='login-form' onSubmit={handleFormSubmit}>
+          <form action="login" className='login-form flex flex-col items-center' onSubmit={handleFormSubmit}>
             <label htmlFor="email-login" className='label-email-login'></label>
             <input type="text" name="email-login" id='email-login' required placeholder='Username/Email' value={email} onChange={(e) => setEmail(e.target.value)}/>
             <label htmlFor='password-login' className='label-password-login'> </label>
             <input type="password" name="password-login" id='password-login' required placeholder='Password           ' value={password} onChange={(e) => setPassword(e.target.value)}/>
-            <p> Forgot your password?</p>
             {/* <input type="password" name="password-login" id='password-login' required placeholder=' Password' value={password} onChange={(e) => setPassword(e.target.value)}/> */}
             {error && <p className="error-message">{error}</p>}
             <button type="submit" className='button-form-login'> SIGN IN NOW <FontAwesomeIcon icon={faChevronRight} /> </button>
